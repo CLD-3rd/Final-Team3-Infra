77afe5fa56322d1cf1b72c474e4a8994973ad56e
wonshee <shee2.won@gmail.com>
2025-07-25 15:36:28 +0900

    S3 IaC 코드 테스트

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -84,10 +84,6 @@ output "custom_route_table_ids" {
 # }
 
 # S3 관련 
-output "s3_bucket_id" {
-  description = "The ID of the created S3 bucket"
-  value       = module.s3_bucket.bucket_id
-}
 output "s3_bucket_arn" {
   description = "The ARN of the created S3 bucket"
   value       = module.s3_bucket.bucket_arn
