b12e049fba42a6a61bb129b03cb1ebcc68d73003
wonshee <shee2.won@gmail.com>
2025-07-30 17:33:22 +0900

    refactoring: Route53 Hosting Only

--- infrastructure/modules/route53/main.tf
+++ infrastructure/modules/route53/main.tf
@@ -2,21 +2,3 @@
 resource "aws_route53_zone" "primary" {
   name = var.domain_name
 }
-
-# ALB 정보 조회 (dns name, zone id)
-data "aws_lb" "argocd" {
-  name = var.argocd_alb_dns
-}
-
-# ArgoCD 접근용 서브도메인 A 레코드 (ALIAS) 생성
-resource "aws_route53_record" "argocd" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "argocd.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = data.aws_lb.argocd.dns_name
-    zone_id                = data.aws_lb.argocd.zone_id
-    evaluate_target_health = true
-  }
-}
--- infrastructure/modules/route53/variables.tf
+++ infrastructure/modules/route53/variables.tf
@@ -2,8 +2,3 @@ variable "domain_name" {
   description = "Route53에 등록할 최상위 도메인 이름"
   type        = string
 }
-
-variable "argocd_alb_dns" {
-  description = "ArgoCD ALB Ingress의 DNS 이름"
-  type        = string
-}
