1947cff293e1e59fb70c17fcc23d79b226e12609
wonshee <shee2.won@gmail.com>
2025-08-07 16:50:18 +0900

    refactoring: image bucket 권한 삭제

--- infrastructure/modules/iam/s3_object.tf
+++ infrastructure/modules/iam/s3_object.tf
@@ -45,27 +45,3 @@ resource "aws_s3_bucket_policy" "nlb_logs" {
     ]
   })
 }
-
-resource "aws_s3_bucket_policy" "image_bucket" {
-  bucket = var.image_bucket_name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontLogsToImageBucket",
-        Effect    = "Allow",
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        },
-        Action    = "s3:PutObject",
-        Resource  = "arn:aws:s3:::${var.image_bucket_name}/*",
-        Condition = {
-          StringEquals = {
-            "AWS:SourceAccount" = data.aws_caller_identity.current.account_id
-          }
-        }
-      }
-    ]
-  })
-}
