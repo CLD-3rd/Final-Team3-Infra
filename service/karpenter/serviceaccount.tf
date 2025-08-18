24bf0676b2d0706f4ded0e7a804eb5098c2ee548
kimrma370 <kimrma370@gmail.com>
2025-08-18 17:31:08 +0900

    feat: flowschema 혜택을 받기 위해 karpenter sa namespace 변경

--- service/karpenter/serviceaccount.tf
+++ service/karpenter/serviceaccount.tf
@@ -1,13 +1,7 @@
-# resource "kubernetes_namespace" "karpenter" {
-#   metadata {
-#     name = "karpenter"
-#   }
-# }
-
 resource "kubernetes_service_account" "karpenter" {
   metadata {
     name      = "karpenter"
-    namespace = kubernetes_namespace.karpenter.metadata[0].name
+    namespace = "kube-system"
     annotations = {
       "eks.amazonaws.com/role-arn" = aws_iam_role.karpenter_irsa.arn
     }
